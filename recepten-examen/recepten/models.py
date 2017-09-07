619c1bca66b17e5927d9fcf0731771ed0ca9f079
Mickaël Vangramberen <m.vangramberen@hotmail.com>
2017-09-07 16:23:39 +0200

    Toevoegen van databank functionaliteiten

--- recepten-examen/recepten/models.py
+++ recepten-examen/recepten/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Gerecht(models.Model):
+    naam = models.CharField(max_length=100)
+    ingredienten = models.CharField(max_length=100)
+    bereidtijd = models.CharField(max_length=100)
+    calorieen = models.CharField(max_length=100)
+    
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
--- recepten-examen/recepten/views.py
+++ recepten-examen/recepten/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 
+# Get from database
+
+'''
+from recepten.models import Gerecht
+
+
+dbgerecht = Gerecht(naam='fish and chips', ingredienten="vis en frieten", bereidtijd="1 uur", calorieen="teveel")
+dbgerecht.save()
+
+gerechtenDbEntry = Gerecht.objects.all()
+
+'''
 gerechten= [
     {"naam":"pannekoeken", "ingredienten":"eieren, suiker, melk", "bereidtijd":"15 min", "calorieen":"750"},
     {"naam":"ommelet", "ingredienten":"eieren, zout, peper", "bereidtijd":"10 min", "calorieen":"200"},
@@ -21,15 +33,26 @@ def recept(request):
 
                 gerechten.append({'naam':naam, 'ingredienten': ingredienten, 'bereidtijd':bereidtijd, 'calorieen':calorieen})
 
+                '''
+                dbgerecht = Gerecht(naam=naam, ingredienten=ingredienten, bereidtijd=bereidtijd, calorieen=calorieen)
+                dbgerecht.save()
+
+                gerechtenDbEntry = Gerecht.objects.all()
+                '''
 
                 context = {
+
+                # 'gerechten' : gerechtenDbEntry
                     'gerechten' : gerechten
+
                 }
 
                 return render(request, 'index.html', context)
 
             else:
                 context = {
+
+                    # 'gerechten' : gerechtenDbEntry
                     'gerechten' : gerechten
                 }
                 return render(request, 'index.html', context)
